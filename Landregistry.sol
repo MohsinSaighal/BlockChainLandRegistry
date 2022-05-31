40021088cc8a7bba8684f15deb626ef7f90ce99f
MohsinSaighal <mohsint908@gmail.com>
2022-05-31 16:47:36 +0500

    Update Landregistry.sol

--- Landregistry.sol
+++ Landregistry.sol
@@ -1,106 +1,124 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
+
 contract Verifications{
      constructor ()
     {
      LandInspectorId=msg.sender;
-     LandInspector("Mohsin",21,"LandInsepcctor");
+     landInspector("Mohsin",21,"LandInsepcctor");
     }
 
-     struct LandInspectorDetails
-    {
-       address Id;
-       string _Name;
-       uint _Age;
-       string Designation;
+     struct LandInspectorDetails{
+       address id;
+       string name;
+       uint age;
+       string designation;
      }
      
      address public LandInspectorId;
-     mapping(address=>LandInspectorDetails)public Inspector;
-     mapping (address=>bool)public SellerVerification;
-     mapping (address=>bool)public SellerRejection;
-     mapping (address=>bool)public BuyerVerification;
-     mapping(address=>bool)public BuyerRejection;
+     mapping(address=>LandInspectorDetails)public inspector;
+     mapping(address=>bool)public sellerVerification;
+     mapping(address=>bool)public sellerRejection;
+     mapping(address=>bool)public buyerVerification;
+     mapping(address=>bool)public buyerRejection;
 
      event VerifiedAddress (address _Id);
      event RejectedAddress(address _Id);
 
      modifier onlyLandInspector()
     {
-     require (LandInspectorId==msg.sender,"You are Not LandInspector");
+     require (
+         LandInspectorId==msg.sender,
+         "You are Not LandInspector"
+         );
      _;
-    }
-
-    modifier BuyerIsNotSeller()
-    {
-      require (!SellerVerification[msg.sender],"Seller Cant Purchase");
-      _;
     }
     
     modifier VerifyandRejectBuyer()
     {
-      require(!BuyerRejection[msg.sender],"You are Rejected Cannot Purchase ");
-      require(BuyerVerification[msg.sender]==true,"You are Not Verified");
+      require(
+          !buyerRejection[msg.sender],
+          "You are Rejected Cannot Purchase "
+          );
+      require(
+          buyerVerification[msg.sender]==true,
+          "You are Not Verified"
+          );
       _;
     }
 
-     function LandInspector(
-     string memory _Name,uint _Age,string memory Designation
-     )internal
+     function landInspector(
+     string memory name,
+     uint age,
+     string memory designation
+     )
+     internal
      {
-     Inspector[LandInspectorId]=LandInspectorDetails(msg.sender,_Name,_Age,Designation);
+     inspector[LandInspectorId]=LandInspectorDetails(
+         msg.sender,name,age,designation
+         );
      }
 
-     function VerifySeller(
-     address sellerId
-     )onlyLandInspector()external
+     function verifySeller(
+     address SellerId
+     )
+     external
+     onlyLandInspector()
      {
-       SellerVerification[sellerId]=true;
-       emit VerifiedAddress(sellerId);
+       sellerVerification[SellerId]=true;
+       emit VerifiedAddress(SellerId);
      }
 
-     function RejectSeller(
+     function rejectSeller(
      address _sellerId
-     )onlyLandInspector() external
+     )
+     external
+     onlyLandInspector()
       {
-        SellerRejection[_sellerId]=true;
+        sellerRejection[_sellerId]=true;
         emit RejectedAddress(_sellerId);
       }
 
-     function VerifyBuyer(
+     function verifyBuyer(
      address BuyerId
-     )onlyLandInspector()public
+     )
+     public
+     onlyLandInspector()
      {
-       BuyerVerification[BuyerId]=true;
+       buyerVerification[BuyerId]=true;
        emit VerifiedAddress (BuyerId);
      }
 
-     function RejectBuyer(address BuyerId)onlyLandInspector()public
+     function rejectBuyer(
+     address BuyerId
+     )
+     public
+     onlyLandInspector()
      {
-       BuyerRejection[BuyerId]=true;
-       emit RejectedAddress (BuyerId);
+       buyerRejection[BuyerId]=true;
+       emit RejectedAddress(BuyerId);
      }
      }
  
-  contract SellerandBuyerDetails is Verifications {
-      struct SellerDetails
-     {
-       address Id;
-       string _Name;
-       uint _Age;
-       string _City;
-       uint _CNIC;
-       string _Email;
 
+  contract SellerandBuyerDetails is Verifications {
+
+      struct SellerDetails{
+       address id;
+       string name;
+       uint age;
+       string city;
+       uint cnic;
+       string email;
      }
-     struct BuyerDetails
-    {
-     address Id;
-     string _Name;
-     uint _Age;
-     string _City;
-     uint _CNIC;
-     string _Email;
+
+     struct BuyerDetails{
+     address id;
+     string name;
+     uint age;
+     string city;
+     uint cnic;
+     string email;
      }
    
      mapping (address=>SellerDetails)public SellerMapping;
@@ -108,190 +126,256 @@ contract Verifications{
      mapping (address=>bool)public IsSellerMapping;
      mapping(address=>bool)public IsBuyerMappping;
 
-      event SellerRegistration (address _id);
-      event BuyerRegistration(address _id);
-      function updateSellerInfo (
-        string memory _Name,uint _Age,string memory _City,uint _CNIC,string memory _Email
-      )external
+     event SellerRegistration(address _id);
+     event BuyerRegistration(address _id);
+
+      function registerSeller (
+        string memory name,
+        uint age,
+        string memory city,
+        uint cnic,
+        string memory email
+      )
+      external
       {
         IsSellerMapping[msg.sender]=true;
-        if (IsSellerMapping[msg.sender]){
-          SellerMapping[msg.sender]= SellerDetails(msg.sender,_Name,_Age,_City,_CNIC,_Email);
-        }else {
+        if (SellerMapping[msg.sender].id==msg.sender){
           SellerDetails storage seller =SellerMapping[msg.sender];
-          seller._Name=_Name;
-          seller._Age=_Age;
-          seller._City=_City;
-          seller._CNIC=_CNIC;
-          seller._Email=_Email;
+          seller.name=name;
+          seller.age=age;
+          seller.city=city;
+          seller.cnic=cnic;
+          seller.email=email;
+        } else {
+        SellerMapping[msg.sender]= SellerDetails(
+        msg.sender,name,age,city,cnic,email
+        );
         }
        }
 
-       function updateBuyerInfo (
-        string memory _Name,uint _Age,string memory _City,uint _CNIC,string memory _Email
-       )external
+       function registerBuyer (
+        string memory name,
+        uint age,
+        string memory city,
+        uint cnic,
+        string memory email
+       )
+       external
        {
         IsBuyerMappping[msg.sender]=true;
-        if (IsBuyerMappping[msg.sender]){
-          BuyerMapping[msg.sender]= BuyerDetails(msg.sender,_Name,_Age,_City,_CNIC,_Email);
-        }else {
-          BuyerDetails storage buyer =BuyerMapping[msg.sender];
-          buyer._Name=_Name;
-          buyer._Age=_Age;
-          buyer._City=_City;
-          buyer._CNIC=_CNIC;
-          buyer._Email=_Email;
+        if (BuyerMapping[msg.sender].id==msg.sender){
+         BuyerDetails storage buyer =BuyerMapping[msg.sender];
+          buyer.name=name;
+          buyer.age=age;
+          buyer.city=city;
+          buyer.cnic=cnic;
+          buyer.email=email;
+        }else{
+         
+          BuyerMapping[msg.sender]=BuyerDetails(
+              msg.sender,name,age,city,cnic,email
+              );
         }
       }
       
-
-     function IsSeller(
+     function isSeller(
          address _Id
-         )public view returns(bool)
+         )
+         public view returns(bool)
          {
            if (IsSellerMapping[_Id]){
              return true;
+         }else{
+           return false;
          }
          }
 
-     function IsBuyer(
+     function isBuyer(
        address _Id
-       )public view returns(bool)
+       )
+       public view returns(bool)
          {
            if (IsBuyerMappping[_Id]){
              return true;
+           }else{
+             return false;
            }
            }
 }
 
 contract LandContract is SellerandBuyerDetails {
 
-     enum Verification {NotVerified,Verified}
-     struct LandReg
-     {
-     Verification verify;
-      string _Area;
-      string _City;
-      string _State;
-      uint _LandPrice;
-      uint _PropertyPID;
+     enum landVerification {NotVerified,Verified}
+
+     struct LandReg{
+     landVerification verify;
+      uint landid;
+      string area;
+      string city;
+      string state;
+      uint landprice;
+      uint propertypid;
      } 
 
-     mapping (uint=>LandReg)public Land; 
-     mapping (uint=>address)public LandOwner;
+     mapping (uint=>LandReg)public land; 
+     mapping (uint=>address)public landowner;
 
-     event landOwner (address PreviousOwner,address newowner,uint LandId,uint  amount);
-     event AddedLand(uint _LandId,address owner);
+     event landdetails (
+     address PreviousOwner,
+     address newowner,
+     uint LandId,
+     uint amount);
+     event AddedLand(
+     uint _LandId,
+     address owner
+     );
 
-     modifier VerifiedAndRejectCheck(){
-     require(!SellerRejection[msg.sender],"You Are Rejected Cant Enter Detail");
-     require (SellerVerification[msg.sender]==true,"You are Not Verified"); 
+     modifier buyerIsNotSeller()
+    {
+      require (
+          SellerMapping[msg.sender].id!=msg.sender,
+          "Seller Cant Purchase"
+          );
+      _;
+    }
+     modifier verifiedAndrejectCheck(){
+     require(
+         !sellerRejection[msg.sender],
+         "You Are Rejected Cant Enter Detail"
+         );
+     require (
+         sellerVerification[msg.sender]==true,
+         "You are Not Verified"
+         ); 
      _;
      }
 
-     function EnterLandDetails(
-     uint _LandId,string memory _Area,string memory _City,string memory _State,uint _LandPrice,uint _PropertyPID
-     )external VerifiedAndRejectCheck()
+     function enterLandDetails(
+       uint landid,
+       string memory area,
+       string memory city,
+       string memory state,
+       uint landprice,
+       uint propertypid
+     )
+     external 
+     verifiedAndrejectCheck()
      { 
-
-      Land[_LandId]=LandReg(Verification.NotVerified,_Area,_City,_State,_LandPrice,_PropertyPID);
-      LandOwner[_LandId]=msg.sender;
-      emit AddedLand(_LandId,LandOwner[_LandId]);
-      
+      land[landid]=LandReg(
+      landVerification.NotVerified,landid,
+      area,city,state,landprice,propertypid
+      );
+      landowner[landid]=msg.sender;
+      emit AddedLand(
+         landid,landowner[landid]
+          );
      }
      
-     function VerifyLand(
-     uint _LandId
-     )external  onlyLandInspector
-     {
-       Land[_LandId].verify=Verification.Verified;
-
+     function verifyLand(uint _LandId)external onlyLandInspector{
+       land[_LandId].verify=landVerification.Verified;
      }
 
-     function PurchaseLand(
+     function purchaseLand(
      uint _LandId,uint _amount
-     )external payable VerifyandRejectBuyer
+     )
+     external payable 
+     VerifyandRejectBuyer() buyerIsNotSeller()
      {
-       address  PreviousOwner= LandOwner[_LandId];
+       address previousOwner= landowner[_LandId];
        address newOwner=msg.sender;
-       require(Land[_LandId].verify==Verification.Verified,"Land is Not Verified");
-       require(Land[_LandId]._LandPrice == _amount,"Please Pay full Payment");
-       require (msg.value==_amount,"Please EnterExactAmount");
-       LandOwner[_LandId]=newOwner;
-       payable (PreviousOwner).transfer(_amount);
-        emit landOwner(PreviousOwner,msg.sender,_LandId,_amount);
+       require(
+           land[_LandId].verify==landVerification.Verified,
+           "Land is Not Verified"
+           );
+       require(
+           land[_LandId].landprice == _amount,
+           "Please Pay full Payment"
+           );
+       require (
+           msg.value==_amount,
+           "Please EnterExactAmount"
+           );
+       landowner[_LandId]=newOwner;
+       payable (previousOwner).transfer(_amount);
+        emit landdetails(
+        previousOwner,msg.sender,
+        _LandId,_amount
+        );
        }
 
-     function transferLand(
-     uint _LandId,address _NewOwner
-     )external
+     function transferLand(uint _LandId, address _NewOwner)external
       {
-       require(msg.sender==LandOwner[_LandId],"You are Not A Owner");
-       LandOwner[_LandId]=_NewOwner;
-
+       require(msg.sender==landowner[_LandId],
+       "You are Not A Owner"
+       );
+       landowner[_LandId]=_NewOwner;
       }
           
-     function GetLandDetails(
-          uint LandId
-         )public view returns (
-          uint256,string memory,string memory,string memory,uint256,uint256
+     function getLandDetails(uint LandId)public view returns (
+          uint256,
+          string memory,
+          string memory,
+          string memory,
+          uint256,
+          uint256
          )
-         {
-           
+         {   
         return (
-         Land[LandId]._LandPrice,
-             Land[LandId]._Area,
-             Land[LandId]._City,
-             Land[LandId]._State,
-             Land[LandId]._LandPrice,
-             Land[LandId]._PropertyPID
+             land[LandId].landid,
+             land[LandId].area,
+             land[LandId].city,
+             land[LandId].state,
+             land[LandId].landprice,
+             land[LandId].propertypid
             );
            
         }
 
-        function CheckLandVerification(
-          uint LandId
-        )public view returns (bool)
+        function isLandVerified(
+        uint LandId)
+        public view returns (bool)
         {
-          if((Land[LandId].verify==Verification.Verified))
+          if((land[LandId].verify==landVerification.Verified))
           {
             return true;
+          }else{
+            return false;
           }
         }
 
-        function CheckLandOwner(
-          uint _LandId
-          )public view returns (address)
+        function checkLandOwner(
+          uint landId
+          )
+          public view returns (address)
           {
-            return LandOwner[_LandId];
+            return landowner[landId];
           }
            
-         function CheckLandInspector(
-
-         )public  view returns (
-           address
-           )
+         function CheckLandInspector()
+          public view returns (address)
            {
              return LandInspectorId;
            }
 
-         function GetLandCity(
+         function getLandCity(
            uint city
           )public view returns(string memory)
          {
-           return Land[city]._City;
+           return land[city].city;
          }
 
-         function GetLandPrice(
-           uint LandPrice
+         function getLandPrice(
+           uint landprice
            )public view returns (uint)
          {
-           return Land[LandPrice]._LandPrice;
+           return land[landprice].landprice;
          }
 
-         function GetLandArea(uint LandArea)public view returns (string memory)
+         function getLandArea(uint landarea)
+         public view returns (string memory)
          {
-           return Land[LandArea]._Area;
+           return land[landarea].area;
          }
            
        }
+
